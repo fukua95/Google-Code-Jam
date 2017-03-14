02821806a35fd82532b9548521034dc9f1904d49
fukua95 <736726758@qq.com>
2017-03-14 21:17:36 +0800

    找下规律

---
+++ google code jam 2016/qualification_D_small.cpp
@@ -0,0 +1,34 @@
+                                            
+  //File Name: qualification_D_small.cpp
+  //Created Time: 2017年03月14日 星期二 19时04分26秒
+                                   
+#include <bits/stdc++.h>
+#define LL long long
+using namespace std;
+LL qp(LL x,LL y){
+	LL res = 1;
+	for(;y>0;y>>=1){
+		if(y & 1) res = res * x;
+		x = x * x;
+	}
+	return res;
+}
+int main(){
+	freopen("D-small-practice.in","r",stdin);
+	freopen("output.txt","w",stdout);
+	int t,cas = 1;
+	scanf("%d",&t);
+	while(t--){
+		int k,c,s;
+		scanf("%d %d %d",&k,&c,&s);
+		printf("Case #%d: ",cas++);
+		LL len = qp(k,c - 1);
+//		printf("len = %lld\n",len);
+		for(int i=0;i<k;++i){
+			printf("%lld",len * i + 1);
+			if(i == k - 1) puts("");
+			else printf(" ");
+		}
+	}
+	return 0;
+}
